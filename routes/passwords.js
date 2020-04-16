edf446af9852a9de3a6d569737438622449c89f6
Mike Rivera <michaelstephenrivera@gmail.com>
2020-04-16 13:19:52 -0700

    Created a GET route for api/passwords to get all users passwords

--- routes/passwords.js
+++ routes/passwords.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Password = require('../models/Password');
 
 // @route     GET api/passwords
 // @desc      Get all users passwords
 // @access    Private
-router.get('/', (req, res) => {
-  res.send('Get all passwords');
+router.get('/', auth, async (req, res) => {
+  // res.send('Get all passwords');
+  try {
+    const passwords = await Password.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(passwords);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route     POST api/passwords
