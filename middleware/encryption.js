ef62b159a6c7ecbdd6a5ac5ca69afb04ff0dfe6a
Mike Rivera <michaelstephenrivera@gmail.com>
2020-04-17 17:13:04 -0700

    Completed functionality for encrypting all Password Info for User Passwords before saving in DB

---
+++ middleware/encryption.js
@@ -0,0 +1,31 @@
+'use strict';
+const config = require('config');
+const crypto = require('crypto');
+
+const ENCRYPTION_KEY = config.get('ENCRYPTION_KEY');
+
+const IV_LENGTH = 16;
+
+function encrypt(text) {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv('aes-256-cbc', new Buffer.from(ENCRYPTION_KEY), iv);
+  let encrypted = cipher.update(text);
+
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
+};
+
+function decrypt(text) {
+  const textParts = text.split(':');
+  const iv = new Buffer.from(textParts.shift(), 'hex');
+  const encryptedText = new Buffer.from(textParts.join(':'), 'hex');
+  const decipher = crypto.createDecipheriv('aes-256-cbc', new Buffer.from(ENCRYPTION_KEY), iv);
+  let decrypted = decipher.update(encryptedText);
+
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+  return decrypted.toString();
+};
+
+module.exports = { encrypt, decrypt };
