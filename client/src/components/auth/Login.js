665d2bf4d3d8f8c22794edc1b578ab4eef15c022
Mike Rivera <michaelstephenrivera@gmail.com>
2020-06-22 12:32:47 -0700

    Added abililty to login a user and authenticate with jwt

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import AuthContext from '../../context/auth/authContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Login = () => {
+
+const Login = (props) => {
+  const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
+  const { setAlert } = alertContext;
+  const { loginUser, error, clearErrors, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      // Redirect
+      props.history.push('/');
+    }
+
+    if (error === 'Invalid Credentials: A user with that email does not exist') {
+      setAlert(error, 'danger');
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
+
   const [user, setUser] = useState({
+    name: '',
     email: '',
-    masterPassword: ''
+    masterPassword: '',
+    passwordReminder: ''
   });
 
   const { email, masterPassword } = user;
@@ -12,8 +36,14 @@ const Login = () => {
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log('Login Submit');
-
+    if (email === '' || masterPassword === '') {
+      setAlert('Please fill in all fields', 'danger');
+    } else {
+      loginUser({
+        email,
+        masterPassword
+      });
+    }
   };
 
   return (
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -72,7 +72,29 @@ const AuthState = props => {
   };
 
   // Login User
-  const loginUser = () => console.log('loginUser');
+  const loginUser = async formData => {
+    const config = {
+      header: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.post('api/auth', formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  };
 
   // Logout
   const logoutUser = () => console.log('logoutUser');
