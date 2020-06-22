7902debc5be8217a05ed0f12ff50080c19f315bb
Mike Rivera <michaelstephenrivera@gmail.com>
2020-06-22 12:59:34 -0700

    Added ability to logout user, remove token, and redirect

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
 
 
 const Navbar = ({ title, icon }) => {
+
+  const authContext = useContext(AuthContext);
+
+  const { isAuthenticated, logoutUser, user } = authContext;
+
+  const onLogout = () => {
+    logoutUser();
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li className="nav-item text-light">Hello {user && user.name}</li>
+      <li className="">
+        <a onClick={onLogout} className="nav-link" href="#!">
+          <i className="fas fa-sign-out-alt"></i> <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li className="nav-item">
+        <Link className="nav-link" to='/register'>Register</Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to='/login'>Login</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div className="navbar navbar-dark bg-dark navbar-expand-lg py-4">
       <div className="container">
@@ -14,18 +46,7 @@ const Navbar = ({ title, icon }) => {
         </button>
         <div id="navbarCollapse" className="collapse navbar-collapse w-75">
           <ul className="navbar-nav d-flex align-items-center ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to='/'>Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to='/about'>About</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to='/register'>Register</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to='/login'>Login</Link>
-            </li>
+            {isAuthenticated ? authLinks : guestLinks}
           </ul>
         </div>
       </div>
