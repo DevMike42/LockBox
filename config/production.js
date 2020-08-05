e9e8489b74109cceb596cd48c7c03e687712caf5
Mike Rivera <michaelstephenrivera@gmail.com>
2020-08-04 17:07:40 -0700

    Updated .env with mongoURI

--- config/production.js
+++ config/production.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-  mongoURI: "mongodb+srv://mike123:mike123@lockbox-bnvet.mongodb.net/test?retryWrites=true&w=majority",
-  jwtSecret: "bologna1",
+  mongoURI: process.env.MONGO_URI,
+  jwtSecret: process.env.JWT_SECRET,
   ENCRYPTION_KEY: process.env.ENCRYPTION_KEY
 };
 
